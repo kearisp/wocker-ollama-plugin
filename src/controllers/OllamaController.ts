b838e8b9784677227f46d53ea8355dfb361f6cd2
Kris Papercut <krispcut@gmail.com>
2024-12-06 23:10:45 +0200

    v1.0.0

--- src/controllers/OllamaController.ts
+++ src/controllers/OllamaController.ts
@@ -15,30 +15,72 @@ export class OllamaController {
         protected readonly ollamaService: OllamaService
     ) {}
 
-    @Command("ollama:start")
+    @Command("ollama:create [name]")
+    public async create(
+        @Param("name")
+        name?: string
+    ): Promise<void> {
+        await this.ollamaService.create(name);
+    }
+
+    @Command("ollama:destroy <name>")
+    @Description("Destroys a specified Ollama instance with options to force and confirm deletion.")
+    public async destroy(
+        @Param("name")
+        name: string,
+        @Option("force", {
+            type: "boolean",
+            alias: "f",
+            description: "Force deletion"
+        })
+        force?: boolean,
+        @Option("yes", {
+            type: "boolean",
+            alias: "y",
+            description: "Automatically confirm deletion"
+        })
+        yes?: boolean
+    ): Promise<void> {
+        await this.ollamaService.destroy(name, force, yes);
+    }
+
+    @Command("ollama:ls")
+    @Description("Lists all available Ollama instances in a tabular format")
+    public async list(): Promise<string> {
+        return this.ollamaService.listTable();
+    }
+
+    @Command("ollama:start [name]")
     @Description("Starting ollama service")
     public async start(
+        @Param("name")
+        name?: string,
         @Option("restart", {
             alias: "r",
             description: "Restarting ollama service"
         })
         restart?: boolean
     ): Promise<void> {
-        await this.ollamaService.start(restart);
+        await this.ollamaService.start(name, restart);
     }
 
-    @Command("ollama:stop")
+    @Command("ollama:stop [name]")
     @Description("Stopping ollama service")
-    public async stop(): Promise<void> {
-        await this.ollamaService.stop();
+    public async stop(
+        @Param("name")
+        name?: string
+    ): Promise<void> {
+        await this.ollamaService.stop(name);
     }
 
-    @Command("ollama:run <model>")
-    @Description("")
+    @Command("ollama:run <name> <model>")
+    @Description("Runs ollama model")
     public async run(
+        @Param("name")
+        name: string,
         @Param("model")
         model: string
-    ) {
-        await this.ollamaService.run(model);
+    ): Promise<void> {
+        await this.ollamaService.run(name, model);
     }
 }
---
+++ src/makes/Config.ts
@@ -0,0 +1,71 @@
+import {ConfigCollection} from "@wocker/core";
+
+import {Service, ServiceProps} from "./Service";
+
+
+type ConfigProps = {
+    default?: string;
+    services?: ServiceProps[];
+};
+
+export abstract class Config {
+    public default?: string;
+    public services: ConfigCollection<Service, ServiceProps>;
+
+    public constructor(props: ConfigProps) {
+        const {
+            default: defaultService,
+            services = []
+        } = props;
+
+        this.default = defaultService;
+        this.services = new ConfigCollection(Service, services);
+    }
+
+    public getService(name: string): Service {
+        const service = this.services.getConfig(name);
+
+        if(!service) {
+            throw new Error(`Ollama "${name}" service not found`);
+        }
+
+        return service;
+    }
+
+    public getDefaultService(): Service {
+        if(!this.default) {
+            throw new Error("No services are installed by default");
+        }
+
+        return this.getService(this.default);
+    }
+
+    public getServiceOrDefault(name?: string): Service {
+        if(!name) {
+            return this.getDefaultService();
+        }
+
+        return this.getService(name);
+    }
+
+    public setService(service: Service): void {
+        this.services.setConfig(service);
+
+        if(!this.default) {
+            this.default = service.name;
+        }
+    }
+
+    public unsetService(name: string): void {
+        this.services.removeConfig(name);
+    }
+
+    public toObject(): ConfigProps {
+        return {
+            default: this.default,
+            services: this.services.toArray()
+        };
+    }
+
+    public abstract save(): void;
+}
